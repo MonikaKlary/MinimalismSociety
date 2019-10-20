46a5023a47ba9c3a2baa5dc17535bcd5742ab91b
MonikaKlary <56270226+MonikaKlary@users.noreply.github.com>
2019-10-20 20:32:31 +0100

    Add files via upload
    First commit
    - Added loadNavbar script
    - Added navbar and home page html files
    - Added CSS file

---
+++ minimalismSociety/loadNavbar.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+//$(document).ready(function () {
+//    $("#navbarFrame").load("/navbar.html");
+//});
+
+$.get("/navbar.html", function (data) {
+    $("navbarFrame").replaceWith(data);
+});
