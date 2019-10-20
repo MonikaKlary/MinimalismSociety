909aa26d369fab52fea388d373ef1fc57bff013a
MonikaKlary <56270226+MonikaKlary@users.noreply.github.com>
2019-10-20 21:01:01 +0100

    Fixed loadNavbar error

--- loadNavbar.js
+++ loadNavbar.js
@@ -1,9 +1,9 @@
 // JavaScript source code
 
-$(document).ready(function () {
-    $("#navbarFrame").load("/navbar.html");
-});
+//$(document).ready(function () {
+//    $("#navbarFrame").load("/navbar.html");
+//});
 
 //$.get("/navbar.html", function (data) {
 //    $("navbarFrame").replaceWith(data);
-});
+//});
