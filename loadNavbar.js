06e2dcc641e4a073f2f8a96f13390e184bab1c93
MonikaKlary <56270226+MonikaKlary@users.noreply.github.com>
2019-11-15 15:51:10 +0000

    Delete loadNavbar.js

--- loadNavbar.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-
-//$(document).ready(function () {
-//    $("#navbarFrame").load("/navbar.html");
-//});
-
-//$.get("/navbar.html", function (data) {
-//    $("navbarFrame").replaceWith(data);
-//});
